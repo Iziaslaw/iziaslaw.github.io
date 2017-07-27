de0eb4d01fc7bc593f32058ae434c7942209baec
Iziaslaw Kitezh <khrisfolkrock@gmail.com>
2017-07-27 17:01:41 +0700

    soundlive

---
+++ games/soundlive/main1.js
@@ -0,0 +1,9 @@
+var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'gameDiv', { preload: preload, create: create, update: update, render: render });
+
+function preload() {};
+
+function create() {};
+
+function update() {};
+
+function render() {};
