9b8a8964f57055a898108630d0f91b758c40554a
Iziaslaw Kitezh <khrisfolkrock@gmail.com>
2018-02-21 03:04:34 +0700

    man

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
       console.log("starting");
       let paragraphs = document.getElementsByTagName('p');
       for (let i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].innerHTML = 'kitten';
+        paragraphs[i].innerHTML = 'BANDAR MELAKA PADA TAHUN';
       }
   onmousemove = null;
     })();
